50fcea4093d3332a33580b0247fdcad5a41202a3
Abraham <abrahamramos.info@gmail.com>
2026-02-18 23:46:10 -0800

    chore: package updates

--- src/components/title-details-dialog.tsx
+++ src/components/title-details-dialog.tsx
@@ -271,7 +271,7 @@ export function TitleDetailsDialog({
         <ScrollArea className="flex-1">
           <div className="relative pb-20">
             {title.backdropUrl && (
-              <div className="absolute inset-0 -z-10 h-[500px] overflow-hidden">
+              <div className="absolute inset-0 -z-10 h-125 overflow-hidden">
                 <Image
                   loader={tmdbImageLoader}
                   src={title.backdropUrl}
